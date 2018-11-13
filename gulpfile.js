0691929d5728e1d79f11ec1140658ebb47ab0d88
ti <ti@artedaconstrucao.com>
2018-11-13 16:05:40 -0200

    pastas organizadas

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var  gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+
+// task para o sass
+gulp.task('sass', function(){
+    
+});
+
+// task default gulp
+gulp.task('default', function(){
+    
+});
