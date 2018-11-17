c0eef32ba9f2acedd103dd80721b355d80451492
ti <ti@db-tilab.arte>
2018-11-17 09:13:15 -0200

    inicio aula 11

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+// task para o sass (função que grava alterações do sass para arquivos de css)
+gulp.task('sass', function(){
+    return gulp.src('sass/**/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+}) ;
+
+// task para watch (função que monitora alterações em tempo de execução e grava direto usando a função 'sass')
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass']);
+});
+
+// task default gulp
+gulp.task('default', ['sass', 'watch']);
+
+
+
